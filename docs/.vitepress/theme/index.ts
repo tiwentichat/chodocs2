346062b003ff297e208fa9f508479ea879897a0c
tiwentichat <125454744+tiwentichat@users.noreply.github.com>
2024-03-23 22:28:34 +0800

    test

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,14 @@
 import { inBrowser, useRoute } from 'vitepress'
-import type { EnhanceAppContext, Theme } from 'vitepress'
+import type { useData, EnhanceAppContext, Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+
 import { nextTick, onMounted, watch } from 'vue'
 import busuanzi from 'busuanzi.pure.js'
 import mediumZoom from 'medium-zoom'
 import { registerAnalytics, siteIds, trackPageview } from './plugins/baidutongji'
 import googleAnalytics from './plugins/googleAnalytics'
+
+import { h, watch } from 'vue'
 import './styles/main.css'
 import './styles/global.css'
 import './styles/demo.css'
@@ -51,4 +54,26 @@ const theme: Theme = {
   },
 }
 
-export default theme
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    const props: Record<string, any> = {}
+    // 获取 frontmatter
+    const { frontmatter } = useData()
+
+    /* 添加自定义 class */
+    if (frontmatter.value?.layoutClass) {
+      props.class = frontmatter.value.layoutClass
+    }
+
+    return h(DefaultTheme.Layout, props, {
+      /**
+       * 相关插槽
+       * https://vitepress.dev/guide/extending-default-theme#layout-slots
+       * https://github.com/vuejs/vitepress/blob/main/src/client/theme-default/Layout.vue
+       */
+      'nav-bar-title-after': () => h(MNavVisitor),
+      'doc-after': () => h(MDocFooter),
+      'aside-bottom': () => h(MAsideSponsors)
+    })
+  },
