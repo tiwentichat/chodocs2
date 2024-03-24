e6a5f656336b1b9f610755e2ce88e3feafb689c7
tiwentichat <125454744+tiwentichat@users.noreply.github.com>
2024-03-24 22:09:39 +0800

    1

--- docs/.vitepress/theme/components/CopyRight.vue
+++ docs/.vitepress/theme/components/CopyRight.vue
@@ -51,7 +51,7 @@ function getGithubLink(name: string) {
             target="_blank"
           >CC BY-NC-SA 4.0</a>
           许可协议。转载请注明来自
-          <a href="https://chodocs.cn/" rel="noreferrer" target="_blank">ChoDocs</a></span>！
+          <a href="https://getoffer.help/" rel="noreferrer" target="_blank">GetOffer.Help</a></span>！
       </div>
     </section>
   </div>
