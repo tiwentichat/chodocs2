3b0bc43a7afec855c6e84ad7971969a228e18bb7
tiwentichat <125454744+tiwentichat@users.noreply.github.com>
2024-03-26 00:04:15 +0800

    重要：添加搜索功能

--- docs/.vitepress/algolia.ts
+++ docs/.vitepress/algolia.ts
@@ -1,7 +1,7 @@
 export default {
   appId: 'SDMTOI5R8O',
   apiKey: '48894d00f17a93cee981641dfb08e2a9',
-  indexName: 'getofferhelp',
+  indexName: 'chocn2',
   placeholder: '请输入关键词',
   translations: {
     button: {
